7ab8fb7bf14ff8c549ee284dd431cf31c20f701b
Vishal Kumar <vskkr007@gmail.com>
2020-10-30 11:45:50 +0530

    updated root

--- django_project/settings.py
+++ django_project/settings.py
@@ -121,6 +121,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
@@ -146,6 +147,3 @@ AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
 
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-
-# AWS_S3_REGION_NAME = 'ap-south-1' #change to your region
-# AWS_S3_SIGNATURE_VERSION = 's3v4'
